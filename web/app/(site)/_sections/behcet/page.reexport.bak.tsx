363c3a0fc371622add50c1158c280badc619a413
Dz <you@example.com>
2026-05-17 15:34:06 +0300

    ⚓ MediSea: Cleaned up residual _sections folder and optimized project size

--- web/app/(site)/_sections/behcet/page.reexport.bak.tsx
+++
@@ -1,6 +0,0 @@
-export const revalidate = 0;
-export const dynamic = "force-dynamic";
-import SectionPageView from "../_SectionPage";
-export default function Page(){ return <SectionPageView section="behcet" />; }
-
-
--- web/app/(site)/_sections/layout.tsx
+++
@@ -1,8 +0,0 @@
-export const revalidate = 0;
-export const dynamic = "force-dynamic";
-
-export default function SectionsLayout({ children }: { children: React.ReactNode }) {
-  return children;
-}
-
-
